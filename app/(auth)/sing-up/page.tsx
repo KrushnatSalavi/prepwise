99163df0c107592ad854f0e8d3992477960fba91
Krushnat Salavi <krushnasalavi@gmail.com>
2026-07-18 20:10:59 +0530

    Auth Pages

--- app/(auth)/sing-up/page.tsx
+++ app/(auth)/sing-up/page.tsx
@@ -1,9 +1,12 @@
-import AuthForm from '@/components/AuthForm'
+// import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const page = () => {
   return (
-    <AuthForm type ="sing-up"/>
+    <div>
+     {/* <AuthForm type ="sing-up"/> */}
+
+    </div>
   )
 }
 
