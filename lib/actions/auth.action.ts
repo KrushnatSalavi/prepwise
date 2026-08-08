66f3ed4082d3b8ecc80519f27512551444e87b41
Krushnat Salavi <krushnasalavi@gmail.com>
2026-08-08 19:39:26 +0530

    sign in and sign up

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,5 +1,11 @@
-'use server'
+'use server';
 
 export async function signUp(params:SignUpParams){
-    const 
+    const {uid, name, email} = params;
+    try{
+
+    }catch(e){
+        console.error('Error creating a user', e);
+        
+    }
 }
