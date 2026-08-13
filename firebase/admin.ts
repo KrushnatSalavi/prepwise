93b5bbdb06e31ebc8a5a79b970a9929bbac0f510
Krushnat Salavi <krushnasalavi@gmail.com>
2026-08-13 23:42:47 +0530

    Debugging

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,18 +1,32 @@
-import { initializeApp, getApps, cert } from "firebase-admin/app";
+import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
-// Initialize Firebase Admin SDK
-function initFirebaseAdmin() {
+const initFirebaseAdmin = () => {
   const apps = getApps();
 
   if (!apps.length) {
+    const projectId = process.env.FIREBASE_PROJECT_ID;
+    const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+    if (!projectId) {
+      throw new Error("FIREBASE_PROJECT_ID is missing");
+    }
+
+    if (!clientEmail) {
+      throw new Error("FIREBASE_CLIENT_EMAIL is missing");
+    }
+
+    if (!privateKey) {
+      throw new Error("FIREBASE_PRIVATE_KEY is missing");
+    }
+
     initializeApp({
       credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // Replace newlines in the private key
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        projectId,
+        clientEmail,
+        privateKey: privateKey.replace(/\\n/g, "\n"),
       }),
     });
   }
@@ -21,6 +35,6 @@ function initFirebaseAdmin() {
     auth: getAuth(),
     db: getFirestore(),
   };
-}
+};
 
 export const { auth, db } = initFirebaseAdmin();
