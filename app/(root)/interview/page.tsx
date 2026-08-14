9e2cf899ba9ef4577cbff657843e91c7f836c511
Krushnat Salavi <krushnasalavi@gmail.com>
2026-08-14 20:21:45 +0530

    Interview Genration UI

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,9 +1,13 @@
+import Agent from '@/components/Agent'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      
+      <h3>
+        InterView Generation 
+      </h3>
+      <Agent userName='John Doe' userId="12345" type='genrate'/>
     </div>
   )
 }
